36d3e09b6719f949284b819942370a14b615b77f
steve hiehn <stevehiehn@gmail.com>
2024-02-29 16:31:23 -0800

    wip - added groups

--- src/dawnet_cli/cli.py
+++ src/dawnet_cli/cli.py
@@ -2,45 +2,50 @@ import click
 from questionary import select
 from .api import get_remotes
 
-@click.group()
-def cli():
-    """DAWnet Command Line Interface."""
-    pass
 
-def list_remotes():
-    for remote in get_remotes():
-        print(remote)
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        base_options(ctx)
+
+def base_options(ctx):
+    """Prompt user for base options if no command is provided."""
+    entry_options = ['tokens', 'remotes']
+    selected_entry_option = select(
+        "Would you like to manage:",
+        choices=entry_options,
+    ).ask()  # .ask() displays the prompt
+
+    # Directly invoke the corresponding command based on the user's selection
+    if selected_entry_option == 'tokens':
+        ctx.invoke(tokens)
+    elif selected_entry_option == 'remotes':
+        ctx.invoke(remotes)
 
 @cli.command()
-def remotes():
-    """Manage DAWnet Remotes."""
-    remote_options = ['list', 'run']
-    # Use questionary to let the user select a remote from the list
-    selected_remote = select(
-        "Select a action:",
+def tokens():
+    """Manage DAWnet Tokens."""
+    click.echo("Token management not implemented.")
+
+@cli.command()
+@click.pass_context
+def remotes(ctx):
+    """What would you like to do with the remotes?"""
+    remote_options = ['list', 'menu']
+    selected_remote_option = select(
+        "Select an action:",
         choices=remote_options,
     ).ask()  # .ask() displays the prompt
 
-    if selected_remote == 'list':  # If a selection was made
-        list_remotes()
-    elif selected_remote == 'run':
-        """Manage DAWnet Remotes."""
-        remote_list = get_remotes()
-        # Use questionary to let the user select a remote from the list
-        selected_remote = select(
-            "Select a remote:",
-            choices=remote_list,
-        ).ask()  # .ask() displays the prompt
-
-        if selected_remote:  # If a selection was made
-            print(f"You selected: {selected_remote}")
-        else:
-            print("No selection was made.")
-
-@cli.command()
-def greet():
-    """Display the welcome message."""
-    print('Welcome to the DAWnet CLI!')
+    if selected_remote_option == 'list':
+        for remote in get_remotes():
+            click.echo(remote)
+    elif selected_remote_option == 'run':
+        click.echo("Remote run action not implemented.")
+    elif selected_remote_option == 'menu':
+        ctx.invoke(cli())
+        #pass
 
 if __name__ == '__main__':
     cli()
