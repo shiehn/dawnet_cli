f38f81bda530c311dc6c147791a8901563c560b4
Steve Hiehn <stevehiehn@Steves-Mac-mini.hitronhub.home>
2024-03-22 23:25:06 -0700

    v0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="runes-cli",
-    version="0.0.2",
+    version="0.2.1",
     author="Steve Hiehn",
     author_email="stevehiehn@gmail.com",
     packages=find_packages(where="src"),
